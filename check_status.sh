64244978d290cc9b89f63f3d06605d9501aa0682
Alvin-yang <perfectyc@126.com>
2023-06-12 22:18:31 +0800

    Update check_status.sh

--- check_status.sh
+++ check_status.sh
@@ -1,7 +1,1 @@
-go run /root/saturn/check_status.go > /root/saturn/status.txt
 
-cat /root/saturn/status.txt | grep -w   "active"  >  /root/saturn/status_active.txt
-cat /root/saturn/status.txt | grep -w   "inactive" > /root/saturn/status_inactive.txt
-cat /root/saturn/status.txt | grep -w   "down"  > /root/saturn/status_down.txt
-cat /root/saturn/status.txt | grep -w   "found"  > /root/saturn/status_no_found.txt
-cat /root/saturn/status_active.txt && cat /root/saturn/status_inactive.txt && cat /root/saturn/status_down.txt && cat /root/saturn/status_no_found.txt
